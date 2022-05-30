ed2d713bca15412c0a8ce624efe7eab2aaf0a099
Candy Fair <candice.bastien@gmail.com>
2022-05-30 17:18:05 +0200

    Style Funding and add mixins

--- src/components/Funding/index.js
+++ src/components/Funding/index.js
@@ -2,22 +2,28 @@ import './style.scss';
 
 const Funding = () => (
   <section className="content__wrapper funding">
-    <div>
-      <div>$xxx</div>
-      <div>of $100,000 backed</div>
+    <div className="funding__details">
+      <div>
+        <div className="funding__details__numbers">$89,914</div>
+        <div className="funding__details__desc">of $100,000 backed</div>
+        <div className="funding__details__line"></div>
+      </div>
+
+      <div>
+        <div className="funding__details__numbers">5,007</div>
+        <div className="funding__details__desc">total backers</div>
+        <div className="funding__details__line"></div>
+      </div>
+
+      <div>
+        <div className="funding__details__numbers">56</div>
+        <div className="funding__details__desc">days left</div>
+      </div>
     </div>
 
-    <div>
-      <div>xxx</div>
-      <div>total backers</div>
+    <div className="funding__tracker">
+      <span className="funding__tracker__backed"></span>
     </div>
-
-    <div>
-      <div>xxx</div>
-      <div>days left</div>
-    </div>
-
-    <div>%%%</div>
   </section>
 );
 
