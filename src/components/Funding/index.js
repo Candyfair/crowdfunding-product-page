835fc8e7f9c3915e5f251686918fe72ff7923aa4
Candy Fair <candice.bastien@gmail.com>
2022-06-02 13:20:02 +0200

    Style Funding for desktop

--- src/components/Funding/index.js
+++ src/components/Funding/index.js
@@ -3,21 +3,28 @@ import './style.scss';
 const Funding = () => (
   <section className="content__wrapper funding">
     <div className="funding__details">
-      <div>
-        <div className="funding__details__numbers">$89,914</div>
-        <div className="funding__details__desc">of $100,000 backed</div>
+      <div className="funding__details__texts">
+        <div>
+          <div className="funding__details__numbers">$89,914</div>
+          <div className="funding__details__desc">of $100,000 backed</div>
+        </div>
+
         <div className="funding__details__line"></div>
       </div>
 
-      <div>
-        <div className="funding__details__numbers">5,007</div>
-        <div className="funding__details__desc">total backers</div>
+      <div className="funding__details__texts">
+        <div>
+          <div className="funding__details__numbers">5,007</div>
+          <div className="funding__details__desc">total backers</div>
+        </div>
         <div className="funding__details__line"></div>
       </div>
 
-      <div>
-        <div className="funding__details__numbers">56</div>
-        <div className="funding__details__desc">days left</div>
+      <div className="funding__details__texts">
+        <div>
+          <div className="funding__details__numbers">56</div>
+          <div className="funding__details__desc">days left</div>
+        </div>
       </div>
     </div>
 
