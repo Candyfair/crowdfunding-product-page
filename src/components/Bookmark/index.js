0db1ed5aa7e760f9d2eed2526fb9c531a37292e5
Candy Fair <candice.bastien@gmail.com>
2022-06-08 18:00:25 +0200

    Add hover on SVG image

--- src/components/Bookmark/index.js
+++ src/components/Bookmark/index.js
@@ -1,19 +1,18 @@
 import './style.scss';
 
-import bookmark from '../../assets/images/icon-bookmark.svg';
-
 const Bookmark = () => (
   <div className="intro__buttons">
     <button type="button" className="content__button intro__buttons__button">Back this project</button>
 
     {/* Mobile */}
     <div className="intro__buttons__bookmark mobile">
-      <img src={bookmark} alt="Bookmark button" />
+      <span className="icon icon-bookmark"></span>
     </div>
 
     {/* Desktop */}
     <div className="intro__buttons__bookmark-desktop desktop">
-      <img src={bookmark} className="intro__buttons__bookmark__img" alt="Bookmark button" />
+      <svg width="56" height="56" xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd"><circle fill="#2F2F2F" cx="28" cy="28" r="28" className="icon" /><path fill="#B1B1B1" d="M23 19v18l5-5.058L33 37V19z" /></g></svg>
+
       <span className="intro__buttons__bookmark__text">Bookmark</span>
     </div>
 
