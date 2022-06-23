2a70563f91c9a082d6e982b049b24b3f8b3a4e13
Candy Fair <candice.bastien@gmail.com>
2022-06-23 19:43:42 +0200

    Add regex to format currency

--- src/components/Funding/index.js
+++ src/components/Funding/index.js
@@ -1,40 +1,46 @@
 import PropTypes from 'prop-types';
 
+import currencyFormat from '../../utils/utils';
+
 import './style.scss';
 
-const Funding = ({ target, date }) => (
-  <section className="content__wrapper funding">
-    <div className="funding__details">
-      <div className="funding__details__texts">
-        <div>
-          <div className="funding__details__numbers">$89,914</div>
-          <div className="funding__details__desc">of $100,000 backed</div>
+const Funding = ({ target, date }) => {
+  const total = currencyFormat(target);
+
+  return (
+    <section className="content__wrapper funding">
+      <div className="funding__details">
+        <div className="funding__details__texts">
+          <div>
+            <div className="funding__details__numbers">$89,914</div>
+            <div className="funding__details__desc">of {total} backed</div>
+          </div>
+
+          <div className="funding__details__line"></div>
         </div>
 
-        <div className="funding__details__line"></div>
-      </div>
-
-      <div className="funding__details__texts">
-        <div>
-          <div className="funding__details__numbers">5,007</div>
-          <div className="funding__details__desc">total backers</div>
+        <div className="funding__details__texts">
+          <div>
+            <div className="funding__details__numbers">5,007</div>
+            <div className="funding__details__desc">total backers</div>
+          </div>
+          <div className="funding__details__line"></div>
         </div>
-        <div className="funding__details__line"></div>
-      </div>
 
-      <div className="funding__details__texts">
-        <div>
-          <div className="funding__details__numbers">56</div>
-          <div className="funding__details__desc">days left</div>
+        <div className="funding__details__texts">
+          <div>
+            <div className="funding__details__numbers">56</div>
+            <div className="funding__details__desc">days left</div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className="funding__tracker">
-      <span className="funding__tracker__backed"></span>
-    </div>
-  </section>
-);
+      <div className="funding__tracker">
+        <span className="funding__tracker__backed"></span>
+      </div>
+    </section>
+  );
+}
 
 Funding.propTypes = {
   target: PropTypes.number.isRequired,
