cf01381d90ad0611a0dc63a058fc171f8c48a839
Candy Fair <candice.bastien@gmail.com>
2022-06-23 18:08:29 +0200

    Add and style ThankYou modal

--- src/components/ModalDisplay/ThankYou/index.js
+++ src/components/ModalDisplay/ThankYou/index.js
@@ -1,4 +1,5 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setModal } from '../../../redux/actions/modals';
 
 import './style.scss';
 import check from '../../../assets/images/icon-check.svg';
@@ -7,15 +8,30 @@ const ThankYou = () => {
   const { element } = useSelector((state) => state.modals);
   const { amount } = useSelector((state) => state.pledge);
 
+  const dispatch = useDispatch();
+  const closeModal = () => {
+    dispatch(setModal(false, ''));
+  };
+
   if (element !== 'thankyou') return null;
 
   return (
-    <div>
-      <div>
-        <img src={check} alt="Check" />
+    <div className="confirmation">
+      <div className="confirmation__img-wrapper">
+        <img src={check} alt="Check" className="confirmation__img" />
       </div>
-      <h1>Thank You!</h1>
-      <p>Amount passed: {amount}</p>
+      <h1 className="confirmation__title">Thanks for your support!</h1>
+      <p className="confirmation__text">Your pledge brings us one step closer to sharing Mastercraft Bamboo Monitor Riser worldwide. You will get an email once our campaign is completed.</p>
+
+      <button
+        className="content__button confirmation__button"
+        type="button"
+        onClick={closeModal}
+      >
+        Got it!
+      </button>
+
+      <p className="error">Amount passed: {amount}</p>
     </div>
   );
 };
